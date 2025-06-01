4fb7fb357a1c2159ea9f47765b6385606d78c792
EmilianoTavera <110911486+EmilianoTavera@users.noreply.github.com>
2025-05-31 23:26:53 -0600

    Update SysUserController.java

--- src/main/java/life/pahtlicoo/infrastructure/controller/SysUserController.java
+++ src/main/java/life/pahtlicoo/infrastructure/controller/SysUserController.java
@@ -12,21 +12,26 @@ import jakarta.ws.rs.core.Response;
 import life.pahtlicoo.application.dto.sysuser.CreateSysUserReqDTO;
 import life.pahtlicoo.application.usecase.sysuser.CreateSysUserUseCase;
 import life.pahtlicoo.domain.model.SysUser;
+import life.pahtlicoo.shared.annotation.NoAuthRequired;
 
 @Path("/sysUser")
 public class SysUserController {
     @Inject
     CreateSysUserUseCase createUserUseCase;
+    //agregar otros métodos de user
 
     @POST
     @Path("/createUser")
+    @NoAuthRequired//Al ser nueva, no tiene forma de verificar
     public Response createUser(CreateSysUserReqDTO createUserReqDTO) {
         try {
             SysUser sysUser = createUserUseCase.execute(createUserReqDTO);
-            return Response.ok(sysUser).build();
+            if (sysUser != null) {
+                return Response.status(Response.Status.CREATED).build();
+            }
+            return Response.status(Response.Status.BAD_REQUEST).build();
         }catch (Exception e){
-            return Response.serverError().entity(e.getMessage()).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
-
 }
