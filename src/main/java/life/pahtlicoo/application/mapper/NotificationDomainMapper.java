e65446304d21facc139a02b9bb30e9940b32c4c0
LuisSalazar02 <luissalazar2002@hotmail.com>
2025-06-06 15:50:07 -0600

    feat: implmented notification creation after shipment order creation

--- src/main/java/life/pahtlicoo/application/mapper/NotificationDomainMapper.java
+++ src/main/java/life/pahtlicoo/application/mapper/NotificationDomainMapper.java
@@ -3,6 +3,7 @@ package life.pahtlicoo.application.mapper;
 import jakarta.enterprise.context.ApplicationScoped;
 import life.pahtlicoo.domain.model.Notification;
 import life.pahtlicoo.domain.model.Request;
+import life.pahtlicoo.domain.model.ShipmentOrder;
 
 @ApplicationScoped
 public class NotificationDomainMapper {
@@ -13,6 +14,16 @@ public class NotificationDomainMapper {
         notification.setReceiverId(2);
         notification.setRequestPhase(1);
         notification.setRequestId(request.getRequestId());
+        return notification;
+    }
+
+    public Notification shipmentOrderToNotification(ShipmentOrder shipmentOrder) {
+        Notification notification = new Notification();
+        notification.setDescription("administrador creó una orden de envío");
+        notification.setSenderId(2);
+        notification.setReceiverId(3);
+        notification.setRequestPhase(2);
+        notification.setRequestId(shipmentOrder.getRequestId());
         return notification;
     }
 }
--- src/main/java/life/pahtlicoo/application/usecase/shipmentorder/CreateShipmentOrderUseCase.java
+++ src/main/java/life/pahtlicoo/application/usecase/shipmentorder/CreateShipmentOrderUseCase.java
@@ -3,8 +3,11 @@ package life.pahtlicoo.application.usecase.shipmentorder;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import life.pahtlicoo.application.dto.shipmentorder.CreateShipmentOrderReqDTO;
+import life.pahtlicoo.application.mapper.NotificationDomainMapper;
 import life.pahtlicoo.application.mapper.ShipmentOrderDomainMapper;
+import life.pahtlicoo.application.service.NotificationService;
 import life.pahtlicoo.application.service.ShipmentOrderService;
+import life.pahtlicoo.domain.model.Notification;
 import life.pahtlicoo.domain.model.ShipmentOrder;
 
 @ApplicationScoped
@@ -15,9 +18,18 @@ public class CreateShipmentOrderUseCase {
     @Inject
     ShipmentOrderDomainMapper shipmentOrderDomainMapper;
 
+    @Inject
+    NotificationService notificationService;
+
+    @Inject
+    NotificationDomainMapper notificationDomainMapper;
+
     public void execute(CreateShipmentOrderReqDTO createShipmentOrderReqDTO) {
         ShipmentOrder shipmentOrder = shipmentOrderDomainMapper.createShipmentOrderToDomain(createShipmentOrderReqDTO);
         shipmentOrderService.createShipmentOrder(shipmentOrder);
+
+        Notification notification = notificationDomainMapper.shipmentOrderToNotification(shipmentOrder);
+        notificationService.createNotification(notification);
     }
 
 }
