a963a9e688c2b8198d952f289980ae01507fb062
Adolfo Hernandez <adolhdzf@gmail.com>
2025-06-07 15:27:40 -0600

    feat: register a consumption based on med and site id

---
+++ src/main/java/life/pahtlicoo/application/dto/medsite/RegisterNewMedSiteConsumptionReqDTO.java
@@ -0,0 +1,14 @@
+package life.pahtlicoo.application.dto.medsite;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterNewMedSiteConsumptionReqDTO {
+    private int consumption;
+}
---
+++ src/main/java/life/pahtlicoo/application/usecase/medsite/RegisterNewMedSiteConsumptionUseCase.java
@@ -0,0 +1,15 @@
+package life.pahtlicoo.application.usecase.medsite;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import life.pahtlicoo.application.dto.medsite.RegisterMedSiteConsumptionDTO;
+import life.pahtlicoo.application.service.MedSiteService;
+
+@ApplicationScoped
+public class RegisterNewMedSiteConsumptionUseCase {
+    @Inject
+    MedSiteService medSiteService;
+    public void execute(RegisterMedSiteConsumptionDTO registerMedSiteConsumptionDTO){
+        medSiteService.RegisterNewMedSiteConsumption(registerMedSiteConsumptionDTO);
+    }
+}
