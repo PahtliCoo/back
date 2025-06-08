40a8308c55b2d97ef8cc568928437af052b5df8a
Adolfo Hernandez <adolhdzf@gmail.com>
2025-06-08 16:40:55 -0600

    feat: retrieve all sites for listing purposes

--- src/main/java/life/pahtlicoo/infrastructure/controller/SiteController.java
+++ src/main/java/life/pahtlicoo/infrastructure/controller/SiteController.java
@@ -4,7 +4,13 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import life.pahtlicoo.application.dto.site.GetAllSitesResDTO;
+import life.pahtlicoo.application.usecase.site.GetAllSitesUseCase;
 import life.pahtlicoo.application.usecase.site.GetSiteByIdUseCase;
+import life.pahtlicoo.domain.model.Site;
+import life.pahtlicoo.shared.annotation.NoAuthRequired;
+
+import java.util.List;
 
 @Path("/sites")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,6 +19,8 @@ public class SiteController {
 
     @Inject
     GetSiteByIdUseCase getSiteByIdUseCase;
+    @Inject
+    GetAllSitesUseCase getAllSitesUseCase;
 
     @GET
     @Path("/{id}")
@@ -24,6 +32,17 @@ public class SiteController {
             return Response.status(Response.Status.NOT_FOUND)
                     .entity("{\"error\": \"" + e.getMessage() + "\"}")
                     .build();
+        }
+    }
+
+    @GET
+    @Path("/all")
+    public Response getAllSites() {
+        try{
+            List<GetAllSitesResDTO> sitesList = getAllSitesUseCase.execute();
+            return Response.ok(sitesList).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
